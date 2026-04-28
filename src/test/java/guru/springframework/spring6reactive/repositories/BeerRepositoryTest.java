2c297605e1adc186812ffe43aca6014d63e2c392
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-28 16:12:37 -0600

    Adding a test

---
+++ src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.spring6reactive.repositories;
+
+import guru.springframework.spring6reactive.domain.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.r2dbc.test.autoconfigure.DataR2dbcTest;
+
+import java.math.BigDecimal;
+
+@DataR2dbcTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void TestSaveNewBeer() {
+        beerRepository.save(getTestBeer())
+                .subscribe(System.out::println);
+
+    }
+
+    Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("Space Dust")
+                .beerStyle("IPA")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .upc("123213")
+                .build();
+    }
+}
