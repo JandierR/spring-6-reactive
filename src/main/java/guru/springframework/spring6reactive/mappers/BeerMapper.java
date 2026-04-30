f5caf4a41228299ce79cca3d4a5aac7ed1bbea3f
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-30 11:38:10 -0600

    Some changes

--- src/main/java/guru/springframework/spring6reactive/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/spring6reactive/mappers/BeerMapper.java
@@ -5,10 +5,10 @@ import guru.springframework.spring6reactive.domain.Beer;
 import guru.springframework.spring6reactive.model.BeerDTO;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface BeerMapper {
     Beer beerDtoToBeer(BeerDTO beerDTO);
 
     BeerDTO beerToBeerDto(Beer beer);
-    
+
 }
