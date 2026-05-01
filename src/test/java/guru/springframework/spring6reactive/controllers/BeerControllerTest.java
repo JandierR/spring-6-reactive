ef72e0ba5e03a0ae21d8c79760a6d24dc090b171
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-01 10:14:35 -0600

    Adding TestMethodOrder to maintain an order to the tests.

--- src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -2,13 +2,17 @@ package guru.springframework.spring6reactive.controllers;
 
 import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.repositories.BeerRepositoryTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 @AutoConfigureWebTestClient
 class BeerControllerTest {
@@ -18,6 +22,7 @@ class BeerControllerTest {
 
 
     @Test
+    @Order(999)
     void testDeleteBeer() {
         webTestClient.delete()
                 .uri(BeerController.BEER_PATH_ID, 1)
@@ -26,6 +31,7 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(3)
     void testUpdateBeer() {
         webTestClient.put()
                 .uri(BeerController.BEER_PATH_ID, 1)
@@ -45,6 +51,7 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(1)
     void testGetById() {
 
         webTestClient.get().uri(BeerController.BEER_PATH_ID, 1)
@@ -55,6 +62,7 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(2)
     void testListBeers() {
         webTestClient.get().uri(BeerController.BEER_PATH)
                 .exchange()
